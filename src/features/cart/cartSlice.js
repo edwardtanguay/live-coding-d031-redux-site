9270c218149ea78dc18ceb3c297df29b4f3a95b2
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-01 12:00:38 +0200

    redux works

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,7 +2,8 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
 	items: [],
-	customerName: ''
+	customerName: '',
+	hasCoupon: false
 };
 
 export const cartSlice = createSlice({
@@ -10,13 +11,18 @@ export const cartSlice = createSlice({
 	initialState,
 	reducers: {
 		addItem: (state, action) => {
+			action.payload.discountedPrice = action.payload.price * (state.hasCoupon ? .9 : 1);
 			state.items.push(action.payload);
 		},
 		changeCustomerName: (state, action) => {
 			state.customerName = action.payload;
+		},
+		addCoupon: (state) => {
+			state.hasCoupon = true,
+				state.items.forEach(m=>m.discountedPrice = m.price * .9)
 		}
 	}
 });
 
-export const { add, subtract } = cartSlice.actions;
+export const { addCoupon } = cartSlice.actions;
 export default cartSlice.reducer;
--- src/pages/PageCart.jsx
+++ src/pages/PageCart.jsx
@@ -1,19 +1,36 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCoupon } from '../features/cart/cartSlice';
 
 export const PageCart = () => {
+	const dispatch = useDispatch();
 	const { items } = useSelector((state) => state.cart);
+
+	const formatPrice = (price) => {
+		return new Intl.NumberFormat('de-DE', {
+			style: 'currency',
+			currency: 'EUR',
+		}).format(price);
+	};
 
 	return (
 		<div className="page_cart">
 			<h2>Cart</h2>
+			<div>
+				<button onClick={() => dispatch(addCoupon())}>
+					Add Coupon
+				</button>
+			</div>
+
 			<p>Number of books: {items.length}</p>
 			<ul>
 				{items.map((item, i) => {
 					return (
-						<li key={i}>{item.title} - {item.price}</li>
-					)
+						<li key={i}>
+							{item.title} - {formatPrice(item.price)} - (discount price: {formatPrice(item.discountedPrice)})
+						</li>
+					);
 				})}
 			</ul>
 		</div>
-	)
-}
+	);
+};
